8a0a56a778ed048adb5e031ed12aad19ae594f96
NazarovDs <merl4210@gmail.com>
2021-05-27 02:51:36 +0500

    add github auth

--- server/core/passport.ts
+++ server/core/passport.ts
@@ -10,17 +10,44 @@ passport.use(
             clientSecret: process.env.GITHUB_CLIENT_SECRET,
             callbackURL: "http://localhost:3001/auth/github/callback"
         },
-        async (_: unknown, __: unknown, profile, cb) => {
-            const obj = {
-                fullName: profile.displayName,
-                avatarURL: profile.photos?.[0].value,
-                userName: profile.username,
-                isActive: 0,
-                phone: ''
+        async (_: unknown, __: unknown, profile, done) => {
+            try {
+                //собираем данные пользователя
+                const obj = {
+                    fullName: profile.displayName,
+                    avatarURL: profile.photos?.[0].value,
+                    userName: profile.username,
+                    isActive: 0,
+                    phone: ''
+                }
+                //проверяем есть ли пользователь в базе
+                const findUser = await User.findOne({
+                    where: {
+                        userName: obj.userName
+                    }
+                })
+                //если нет - добавляем
+                if (!findUser) {
+                    const user = await User.create(obj)
+                    return  done(null, user.toJSON())
+                }
+                //если есть - возвращаем
+                done(null, findUser)
+            } catch (e) {
+                done(e)
             }
-            const user = await User.create(obj)
-            cb()
+
         }
     ));
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+
+passport.deserializeUser((id, done) => {
+    User.findById(id, (err,user) => {
+        err ? done(err) : done(null,user);
+    });
+});
 
 export {passport}
--- server/index.ts
+++ server/index.ts
@@ -1,19 +1,22 @@
 import express from 'express'
 import dotenv from 'dotenv'
+
 dotenv.config({path: __dirname + '/.env'})
-import { passport } from './core/passport';
+import {passport} from './core/passport';
 import './core/db'
 
 const app = express()
+app.use(passport.initialize())
 
 app.get('/auth/github', passport.authenticate('github'));
 
 app.get(
     '/auth/github/callback',
-    passport.authenticate('github', { failureRedirect: '/login' }),
+    passport.authenticate('github', {failureRedirect: '/login'}),
     (req, res) => {
-        res.send()
+        res.send(
+            `<script>window.close();window.opener.postMessage(${JSON.stringify(req.user)}, 'http://localhost:3000');</script>`)
     });
-app.listen(3001, ()=> {
+app.listen(3001, () => {
     console.log('SERVER RUNNED')
 })
